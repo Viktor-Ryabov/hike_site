b6c3ea935486f6ed56a5fde4658ed2258f46a29e
Viktor-Ryabov <iprva@ya.ru>
2023-03-18 12:26:45 +0300

    styky header menu

--- src/Modules/App/App.js
+++ src/Modules/App/App.js
@@ -1,18 +1,18 @@
 // import { Fire } from "../Fire/Fire";
 // import { Camp } from "../Camp/Camp";
 import NavbarBrand from "../NavbarBrand/NavbarBrand";
+import { useRef, useLayoutEffect } from "react";
 import Styles from "./App.module.css";
 import TabBarProducts from "../../Pages/Main/Components/TabBarProducts/TabBarProducts";
 import { UtpAndForm } from "../../Pages/Main/Components/UtpAndForm/UtpAndForm";
 
-
 export const App = () => {
     return (
         <div className={Styles.app}>
-            <NavbarBrand />
-            <UtpAndForm>
+            <NavbarBrand>
                 
-            </UtpAndForm>
+            </NavbarBrand>
+            <UtpAndForm></UtpAndForm>
             <TabBarProducts />
         </div>
     );
