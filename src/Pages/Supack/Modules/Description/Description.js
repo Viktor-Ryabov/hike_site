fa978eec78789541175c800cf68ca15d4e5d65a6
Viktor Ryabov <iprva@ya.ru>
2023-05-02 13:03:33 +0300

    added bitoons

--- src/Pages/Supack/Modules/Description/Description.js
+++ src/Pages/Supack/Modules/Description/Description.js
@@ -6,21 +6,21 @@ import ActionButton from "../ActionButton/ActionButton";
 const Description = () => {
     return (
             <section className={Styles.studyComposition} id="studyComposition">
-                <p className={Styles.studyComposition__blockName}>Отличия</p>
-                <h3 className={Styles.studyComposition__title}>
+                <p className={Styles.blockName}>Отличия</p>
+                <h3 className={Styles.title}>
                     Питание на природе может быть полноценным, а так же вкусным
                     и полезным
                 </h3>
                 <img
                     src={fotoDescription}
                     alt="дорога в лесу"
-                    className={Styles.studyComposition__foto}
+                    className={Styles.foto}
                 />
-                <p className={Styles.studyComposition__text}>
+                <p className={Styles.text}>
                     Экологичные рецепты на основе круп, овощей, специй и мяса
                     (опционально).
                 </p>
-                <p className={Styles.studyComposition__text_2}>
+                <p className={Styles.text_2}>
                     Можно сделать как отдельные блюда, так и полноценную систему
                     питания (раскладку) на несколько дней. Блюда при этом
                     практически не будут повторяться.
@@ -28,19 +28,19 @@ const Description = () => {
                 
                 <ActionButton text="Узнать как это?" />
                 
-                <ul className={Styles.studyComposition__rules}>
-                    <li className={Styles.studyComposition__compositionRule}>
+                <ul className={Styles.rules}>
+                    <li className={Styles.compositionRule}>
                         Вес упакованых блюд меньше аналогов, так как овощи и
                         мясо высушены.
                     </li>
-                    <li className={Styles.studyComposition__compositionRule}>
+                    <li className={Styles.compositionRule}>
                         Простота: открыл пакет, высыпал в воду, сварил, готово!
                     </li>
-                    <li className={Styles.studyComposition__compositionRule}>
+                    <li className={Styles.compositionRule}>
                         Рецепты очень вкусные и не содержат консервантов и
                         усилителей вкуса!
                     </li>
-                    <li className={Styles.studyComposition__compositionRule}>
+                    <li className={Styles.compositionRule}>
                         Полноценные продукты лучше сублиматов и продуктов
                         быстрого приготовления
                     </li>
